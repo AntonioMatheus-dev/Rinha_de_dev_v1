6031790da5d2879058067079ced89095413bb108
AntonioMatheus-dev <matheus69969@gmail.com>
2026-03-28 14:36:04 -0300

    feat: implement Fastify server with PostgreSQL connection and ticket reservation logic

--- dist/index.js
+++ dist/index.js
@@ -12,9 +12,9 @@ const fastify = (0, fastify_1.default)({ logger: false });
 const pool = new pg_1.Pool({
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT) || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASS || "mat.1127",
-  database: process.env.DB_NAME || "rinha",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS ,
+  database: process.env.DB_NAME ,
   max: 30,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 3000,
